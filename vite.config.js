b41484b9649959222ed05e9c6671ece6f90c6c5f
rathodvinesh <vineshr4561@gmail.com>
2025-03-28 19:43:57 +0530

    gitpages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rathodvinesh.github.io/', // Important: match your repository name
+  build: {
+    outDir: 'dist'
+  }
 })
