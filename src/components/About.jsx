09138d3da02b090f631b839476b9372ff98799bd
rathodvinesh <vineshr4561@gmail.com>
2025-03-31 21:00:15 +0530

    Fonts Color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                   whileInView={{opacity: 1, y: 0}}
                   initial={{opacity: 0, y: -100}}
                   transition={{duration: 1}}
-            className="font-medium text-gray-300 text-3xl sm:text-6xl md:text-6xl lg:text-6xl mb-8 md:mb-16 lg:mb-24">
+            className="font-medium text-gray-300 text-4xl md:text-3xl lg:text-7xl mb-8 md:mb-24 lg:mb-32">
             Your skills are best shown through results, not numbers
             </motion.h1>
 
@@ -19,7 +19,7 @@ const About = () => {
                 whileInView={{opacity: 1, x: 0}}
                 initial={{opacity: 0, x: -100}}
                 transition={{duration: 1}}
-                className="font-light text-gray-300 text-base sm: md:text-sm lg:text-base"
+                className="font-normal text-gray-300 text-sm sm:text-sm md:text-sm lg:text-base"
             >
                 With a background in computer engineering, I specialize in creating efficient, user-friendly solutions. <br/>I have experience with <b>React.js</b> for frontend, <b>Kotlin</b> for Android apps, and <b>Firebase</b> for data synchronization, <br/>along with a strong foundation in <b>Java</b>.
             </motion.p>
